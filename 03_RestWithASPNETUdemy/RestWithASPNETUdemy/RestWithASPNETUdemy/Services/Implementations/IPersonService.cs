e162923f03518574101d779b34cc9819a19f85a6
jpthimotheo <jpthimotheoo@hotmail.com>
2023-07-13 22:14:21 -0300

    adicionado mais uma aula

---
+++ 03_RestWithASPNETUdemy/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+
+        Person Update(Person person);
+        List<Person> FindAll();
+
+        void Delete(long id);
+
+    }
+}
